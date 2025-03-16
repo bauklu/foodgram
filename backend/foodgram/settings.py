314f25dd318f9ea01e2556f66e53116c1eb821b0
Liudmila <bauklu@mail.ru>
2025-03-16 21:51:27 +0300

    CSRF_TRUSTED_ORIGINS

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CSRF_TRUSTED_ORIGINS = ['https://taskilearn.hopto.org']
+CSRF_TRUSTED_ORIGINS = ['https://taskilearn.hopto.org']
 
 ROOT_URLCONF = 'foodgram.urls'
 
