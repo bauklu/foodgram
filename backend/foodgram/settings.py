a1669cf9ae9767b9eada2c3877701bfd44969558
Liudmila <bauklu@mail.ru>
2025-03-17 13:53:47 +0300

    admin.py changed

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -108,8 +108,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = 'backend_static/'
-# STATIC_URL = 'static/'
+# STATIC_URL = 'backend_static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
