8f328fb7fca30569b89764561cc48ad01b195ff6
Liudmila <bauklu@mail.ru>
2025-03-18 10:09:19 +0300

    debug=false

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,8 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'SECRET')
 
-# DEBUG = (os.environ.get('DEBUG') == 'True')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
