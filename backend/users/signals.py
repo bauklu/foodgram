79402f218caf363b6958127b7340d49861c3bdf7
Liudmila <bauklu@mail.ru>
2025-11-29 19:04:32 +0300

    bugs fixed

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
+
 @receiver(post_migrate)
 def create_superuser(sender, **kwargs):
     if os.environ.get("AUTO_SUPERUSER") != "true":
